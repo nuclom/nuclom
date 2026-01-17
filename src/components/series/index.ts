30580243e5599d152fc72f7d910662bc74b58780
Alexis Rico <sferadev@gmail.com>
2026-01-17 22:27:41 +0100

    Clean-up microfrontends (#307)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- src/components/series/index.ts
+++
@@ -1,4 +0,0 @@
-export { SeriesCard } from './series-card';
-export { SeriesForm } from './series-form';
-export { SortableVideoList } from './sortable-video-list';
-export { VideoPicker } from './video-picker';
--- src/lib/email.ts
+++
@@ -1,4 +0,0 @@
-import { Resend } from 'resend';
-import { env } from './env/server';
-
-export const resend = new Resend(env.RESEND_API_KEY);
