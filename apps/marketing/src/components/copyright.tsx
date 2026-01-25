274ad69fedf0ce4b093f1404fbe876a78e8591c3
Alexis Rico <sferadev@gmail.com>
2026-01-25 10:46:06 +0100

    Update copy right
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- apps/marketing/src/components/copyright.tsx
+++ apps/marketing/src/components/copyright.tsx
@@ -1,17 +1,3 @@
-import { connection } from 'next/server';
-import { Suspense } from 'react';
-
-const CURRENT_YEAR = 2026;
-
-async function CopyrightAsync() {
-  await connection();
-  return <>© {new Date().getFullYear()} Nuclom. All rights reserved.</>;
-}
-
 export function Copyright() {
-  return (
-    <Suspense fallback={`© ${CURRENT_YEAR} Nuclom. All rights reserved.`}>
-      <CopyrightAsync />
-    </Suspense>
-  );
+  return <>© 2026 Nuclom. All rights reserved.</>;
 }
